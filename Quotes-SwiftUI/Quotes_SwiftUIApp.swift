7df61c8245bf1d98ea3d1f3e7e5590a7d70e3497
Raja Adeel Ahmed <adeel.ahmed@telenorbank.pk>
2023-04-06 08:52:44 +0500

    Initial Commit

---
+++ Quotes-SwiftUI/Quotes_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quotes_SwiftUIApp.swift
+//  Quotes-SwiftUI
+//
+//  Created by Raja Adeel Ahmed on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Quotes_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
